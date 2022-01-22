2caf44564dc6669ac435736b79871ac2aef1b731
Dave Quinn <dquinn8@cogeco.ca>
2022-01-22 13:01:05 -0500

    imported redux actions into CategoryMenu component

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,61 +1,81 @@
 import React, { useEffect } from 'react';
-import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+//import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
-import { useStoreContext } from "../../utils/GlobalState";
+//import { useStoreContext } from "../../utils/GlobalState";
 import { idbPromise } from '../../utils/helpers';
 
-function CategoryMenu() {
+import { connect } from "react-redux";
+import { updateCategories, updateCurrentCategory } from '../../redux/actions';
 
-  const [state, dispatch] = useStoreContext();
 
-  const { categories } = state;
-  
-  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
-  
-  useEffect(() => {
-    // if categoryData exists or has changed from the response of useQuery, then run dispatch()
-    if (categoryData) {
-      // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
-      dispatch({
-        type: UPDATE_CATEGORIES,
-        categories: categoryData.categories
-      });
-      categoryData.categories.forEach(category => {
-        idbPromise('categories', 'put', category);
-      });
-    } else if (!loading) {
-      idbPromise('categories', 'get').then(categories => {
+function CategoryMenu({ state, updateCategories, updateCurrentCategory }) {
+
+    //const [state, dispatch] = useStoreContext();
+
+    const { categories } = state;
+
+    const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+    useEffect(() => {
+        // if categoryData exists or has changed from the response of useQuery, then run dispatch()
+        if (categoryData) {
+            // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
+            /* dispatch({
+               type: UPDATE_CATEGORIES,
+               categories: categoryData.categories
+             });
+             */
+            updateCategories(categoryData.categories);
+            categoryData.categories.forEach(category => {
+                idbPromise('categories', 'put', category);
+            });
+        } else if (!loading) {
+            idbPromise('categories', 'get').then(categories => {
+                /*
+                dispatch({
+                  type: UPDATE_CATEGORIES,
+                  categories: categories
+                });
+                */
+                updateCategories(categories);
+            });
+        }
+    }, [categoryData, loading, updateCategories]);
+
+    const handleClick = id => {
+        /* 
         dispatch({
-          type: UPDATE_CATEGORIES,
-          categories: categories
-        });
-      });
-    }
-  }, [categoryData, loading, dispatch]);
+           type: UPDATE_CURRENT_CATEGORY,
+           currentCategory: id
+         });
+         */
+        updateCurrentCategory(id);
+    };
 
-  const handleClick = id => {
-    dispatch({
-      type: UPDATE_CURRENT_CATEGORY,
-      currentCategory: id
-    });
-  };
-
-  return (
-    <div>
-    <h2>Choose a Category:</h2>
-    {categories.map(item => (
-      <button
-        key={item._id}
-        onClick={() => {
-          handleClick(item._id);
-        }}
-      >
-        {item.name}
-      </button>
-    ))}
-  </div>
-  );
+    return ( <
+        div >
+        <
+        h2 > Choose a Category: < /h2> {
+            categories.map(item => ( <
+                button key = { item._id }
+                onClick = {
+                    () => {
+                        handleClick(item._id);
+                    }
+                } >
+                { item.name } <
+                /button>
+            ))
+        } <
+        /div>
+    );
 }
 
-export default CategoryMenu;
+const mapStateToProps = state => {
+    return { state: state };
+};
+// export default VisibilityFilters;
+export default connect(
+    mapStateToProps, { updateCategories, updateCurrentCategory }
+)(CategoryMenu);
